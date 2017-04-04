eeb71135473a5dd71a9f51a1138fe1829e7c2f59
Benjamin-pc <Benjamin-pc@DESKTOP-CCK239H>
2017-04-04 16:51:05 +0200

    Add project files.

---
+++ HoteldatabaseFrontEnd_testafprojekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoteldatabaseFrontEnd_testafprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
