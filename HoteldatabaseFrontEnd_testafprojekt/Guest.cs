595048a70d7a4eb2a262f2e6035f3e8eadebeab9
Benjamin-pc <Benjamin-pc@DESKTOP-CCK239H>
2017-04-06 09:15:47 +0200

    Oprettede og testede for post

---
+++ HoteldatabaseFrontEnd_testafprojekt/Guest.cs
@@ -0,0 +1,17 @@
+﻿namespace HoteldatabaseFrontEnd_testafprojekt
+{
+
+    public partial class Guest
+    {
+        public Guest()
+        {
+        }
+
+        public int Guest_No { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
--- HoteldatabaseFrontEnd_testafprojekt/Program.cs
+++ HoteldatabaseFrontEnd_testafprojekt/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace HoteldatabaseFrontEnd_testafprojekt
 {
@@ -10,6 +12,54 @@ namespace HoteldatabaseFrontEnd_testafprojekt
     {
         static void Main(string[] args)
         {
+            var MyNewGuest = new Guest()
+            {
+                Name = "Benjamin",
+                Address = "her"
+            };
+
+            //POST
+            Console.WriteLine("Tester for post");
+            Console.WriteLine();
+
+            const string serverUrl = "http://hotelws.azurewebsites.net";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+
+                try
+                {
+                    var response = client.PostAsJsonAsync<Guest>("api/Values", MyNewGuest).Result;
+                                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Du har indsat et nyt event");
+                    Console.WriteLine("Post context: " + response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    Console.WriteLine("Fejl, Eventet blev ikke oprettet!");
+                    Console.WriteLine("Statuscode: " + response.StatusCode);
+                }
+
+
+            }
+
+
+                catch (Exception e)
+            {
+
+                Console.WriteLine("Der er sket en fejl : " + e.Message);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            }
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
